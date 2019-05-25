2322214bae650f5c3ae48dfd25674204e024a02b
Prometheus <glebus24@mail.ru>
2019-05-25 10:00:16 +0300

    Components added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import PhotoList from './PhotoList';
+
+render(<PhotoList />, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+let path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                loaders: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    }
+};
