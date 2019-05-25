251d0ea3465f766b0d914a747c098dc6c595b94a
Prometheus <glebus24@mail.ru>
2019-05-25 12:53:21 +0300

    PhotoList with CSS

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -2,11 +2,34 @@ import React from 'react';
 
 export default class PhotoList extends React.Component {
 
+    state = {
+        picturesUrl: []
+    };
+
+    componentDidMount() {
+        const arrId = [2, 7, 1, 8, 3, 9];
+        const apiUrl = 'https://picurl.herokuapp.com/users/';
+
+        Promise.all(arrId.map(id =>
+            fetch(apiUrl + id)
+                .then(res => res.json())
+                .catch(error => {
+                    throw error
+                })
+        ))
+            .then(results =>
+                this.setState({
+                    picturesUrl: results
+                })
+            )
+    }
+
     render() {
         return (
-            <ul>
-                <li></li>
-            </ul>
+            <div>
+                {this.state.picturesUrl.map(picture =>
+                    <img key={picture.url} src={picture.url} alt="new" className="round"/>
+                )}
+            </div>
         )
-    }
-}
+    }}
